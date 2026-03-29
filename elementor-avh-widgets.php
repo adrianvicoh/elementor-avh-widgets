dd6f560b15c6923b4ef8231fc2459daa7189770f
Adrián Vico Hernández <adrian.vico.95@gmail.com>
2026-03-29 20:42:15 +0200

    feat: elementor-avh-widgets.php Added main functions, assets and widgets from previous testings

---
+++ elementor-avh-widgets.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name:    Elementor AVH Widgets
+Description:    WordPress plugin with extended widgets for Elementor
+Version:        0.0.1
+Author:         Adrián Vico Hernández <adrian.vico.95@gmail.com>
+License:        GPL v2 or later
+Text Domain:    elementor-avh-widgets
+*/
+
+/**
+ * Register Elementor test widgets.
+ */
+function register_new_widgets($widgets_manager)
+{
+    require_once __DIR__ . "/widgets/widget-1.php";
+
+    $widgets_manager->register(new \Elementor_Widget_1());
+}
+add_action("elementor/widgets/register", "register_new_widgets");
+
+/**
+ * Register categories for Elementor test widgets.
+ */
+function add_elementor_widget_categories($elements_manager)
+{
+    $elements_manager->add_category("wwwarcelona", [
+        "title" => esc_html__("wwwarcelona", "textdomain"),
+        "icon" => "fa fa-plug",
+    ]);
+}
+add_action(
+    "elementor/elements/categories_registered",
+    "add_elementor_widget_categories",
+);
+
+/**
+ * Register scripts and styles for Elementor test widgets.
+ */
+function elementor_test_widgets_dependencies()
+{
+    /* Scripts */
+    wp_register_script(
+        "widget-script-1",
+        plugins_url("assets/js/widget-script-1.js", __FILE__),
+    );
+
+    /* Styles */
+    wp_register_style(
+        "widget-style-1",
+        plugins_url("assets/css/widget-style-1.css", __FILE__),
+    );
+}
+add_action("wp_enqueue_scripts", "elementor_test_widgets_dependencies");
---
+++ widgets/widget-1.php
@@ -0,0 +1,190 @@
+<?php
+class Elementor_Widget_1 extends \Elementor\Widget_Base
+{
+
+    public function get_name(): string
+    {
+        return 'widget_prova';
+    }
+
+    public function get_title(): string
+    {
+        return esc_html__('Widget de prueba', 'textdomain');
+    }
+
+    public function get_icon(): string
+    {
+        return 'eicon-global-settings';
+    }
+
+    public function get_categories(): array
+    {
+        return ['wwwarcelona'];
+    }
+
+    public function get_keywords(): array
+    {
+        return ['keyword', 'keyword'];
+    }
+
+    public function get_custom_help_url(): string
+    {
+        return 'https://wwwarcelona.com/';
+    }
+
+    protected function get_upsale_data(): array
+    {
+        return [];
+    }
+
+    protected function register_controls(): void
+    {
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label' => esc_html__('Content', 'textdomain'),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this->add_control(
+            'title',
+            [
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'label' => esc_html__('Title', 'textdomain'),
+                'placeholder' => esc_html__('Enter your title', 'textdomain'),
+            ]
+        );
+
+        $this->add_control(
+            'size',
+            [
+                'type' => \Elementor\Controls_Manager::NUMBER,
+                'label' => esc_html__('Size', 'textdomain'),
+                'placeholder' => '0',
+                'min' => 0,
+                'max' => 100,
+                'step' => 1,
+                'default' => 50,
+            ]
+        );
+
+        $this->add_control(
+            'open_lightbox',
+            [
+                'type' => \Elementor\Controls_Manager::SELECT,
+                'label' => esc_html__('Lightbox', 'textdomain'),
+                'options' => [
+                    'default' => esc_html__('Default', 'textdomain'),
+                    'yes' => esc_html__('Yes', 'textdomain'),
+                    'no' => esc_html__('No', 'textdomain'),
+                ],
+                'default' => 'no',
+            ]
+        );
+
+        $this->add_control(
+            'alignment',
+            [
+                'type' => \Elementor\Controls_Manager::CHOOSE,
+                'label' => esc_html__('Alignment', 'textdomain'),
+                'options' => [
+                    'left' => [
+                        'title' => esc_html__('Left', 'textdomain'),
+                        'icon' => 'eicon-text-align-left',
+                    ],
+                    'center' => [
+                        'title' => esc_html__('Center', 'textdomain'),
+                        'icon' => 'eicon-text-align-center',
+                    ],
+                    'right' => [
+                        'title' => esc_html__('Right', 'textdomain'),
+                        'icon' => 'eicon-text-align-right',
+                    ],
+                ],
+                'default' => 'center',
+            ]
+        );
+
+        $this->add_control(
+            'image',
+            [
+                'label' => esc_html__('Choose Image', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::MEDIA,
+                'default' => [
+                    'url' => \Elementor\Utils::get_placeholder_image_src(),
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+
+        $this->start_controls_section(
+            'section_style',
+            [
+                'label' => esc_html__('Style', 'textdomain'),
+                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->add_control(
+            'color',
+            [
+                'label' => esc_html__('Color', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'default' => '#f00',
+                'selectors' => [
+                    '{{WRAPPER}} h3' => 'color: {{VALUE}}',
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+    }
+
+    protected function render(): void
+    {
+        $settings = $this->get_settings_for_display();
+
+        if (empty($settings['title'])) {
+            return;
+        }
+?>
+        <h3>
+            <?php echo $settings['title']; ?>
+        </h3>
+    <?php
+
+        echo \Elementor\Group_Control_Image_Size::get_attachment_image_html($settings);
+    }
+
+    protected function content_template(): void
+    {
+    ?>
+        <#
+            if ( ''===settings.title ) {
+            return;
+            }
+            #>
+            <h3>
+                {{{ settings.title }}}
+            </h3>
+            <#
+                const image={
+                id: settings.image.id,
+                url: settings.image.url,
+                size: settings.image_size,
+                dimension: settings.image_custom_dimension,
+                model: view.getEditModel()
+                };
+
+                const image_url=elementor.imagesManager.getImageUrl( image );
+
+                if ( ''===image_url ) {
+                return;
+                }
+                #>
+                <img src="{{{ image_url }}}">
+        <?php
+    }
+}
